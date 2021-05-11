46c86a28386b186d9f0fe2a313704736fdb920b0
piccinnigius <piccinninnigius@gmail.com>
2021-05-12 00:14:36 +0200

    esempio sass

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </header>
+      <header className="header header2">
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
       </header>
     </div>
   );
