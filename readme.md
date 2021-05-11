c9cfd86fdf550ad4d1aa0fec466424a427633abc
piccinnigius <piccinninnigius@gmail.com>
2021-05-12 00:08:53 +0200

    sass + rework stili

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 installare node 16
 npm config get prefix -> aggiungere alla path
 npm install -g yarn
+yarn create react-app ./nome-app
+cd ./nome-app
+yarn start
+yarn add sass
+assets->convertire css
--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
