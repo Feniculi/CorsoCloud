d188428da63f2b17849bb0ad7d9f7f9d069cad36
piccinnigius <piccinninnigius@gmail.com>
2021-05-12 23:00:58 +0200

    grafica search

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.scss'
+import wiki from 'wikipedia'
+
+( async () => {
+  try {
+    const summary = await wiki.summary( 'Batman' );
+    console.log( summary );
+    //Response of type @wikiSummary - contains the intro and the main image
+  } catch ( error ) {
+    console.log( error );
+    //=> Typeof wikiError
+  }
+} )();
 
 export const Chat = () => {
+  const [messages, setMessages] = useState( [] )
 
   return (
     <div className="chat-container">
-      Chat
+      <div className="messages">
+        {
+          messages.length === 0 ? <h2>Non ci sono messaggi qui...</h2> :
+            <div />
+        }
+      </div>
+      <div className="search">
+        <div class="form-group field">
+          {
+            //autoComplete="off"
+          }
+          <input type="input" autoComplete="off" class="form-field" placeholder="Name" name="name" id='name' required />
+          <label for="name" class="form-label">Search</label>
+          <button className="primary">{'>'}</button>
+        </div>
+      </div>
     </div>
   )
 }
