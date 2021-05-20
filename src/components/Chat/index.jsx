e68188b7522310a7a9c1c6a5ea6523ba20cfb039
piccinnigius <piccinninnigius@gmail.com>
2021-05-20 19:30:35 +0200

    Update index.jsx

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -3,33 +3,35 @@ import './styles.scss'
 import wikipedia from 'helpers/wikipedia'
 import { Card } from 'components/Card'
 
-export const Chat = () => {
+export const Chat = ( props ) => {
   const [messages, setMessages] = useState( [] )
   const [search, setSearch] = useState( "" )
 
-  /* const searchCurrent = async () => {
-     const toSearch = search
-     setMessages( prev => [...prev, { text: search, personal: true }] )
-     setSearch( "" )
-     try {
-       const res = await wikipedia.summary( toSearch )
-       setMessages( prev => [...prev, {
-         title: res.displaytitle || res.title,
-         text: res.extract,
-         imgUrl: res?.thumbnail?.source || res?.originalImage?.source,
-         actionUrl: res?.content_urls?.desktop?.page,
-         response: res
-       }] )
-     } catch ( e ) {
-       console.log( e.message, toSearch )
-     }
-   }*/
+  const searchCurrent = async () => {
+    const toSearch = search
+    setMessages( prev => [...prev, { text: search, personal: true }] )
+    setSearch( "" )
+    try {
+      const res = await wikipedia.summary( toSearch )
+      console.log( 'API_RESULT', res )
+      setMessages( prev => [...prev, {
+        title: res.displaytitle || res.title,
+        text: res.extract,
+        imgUrl: res?.thumbnail?.source || res?.originalImage?.source,
+        actionUrl: res?.content_urls?.desktop?.page,
+        response: res
+      }] )
+    } catch ( e ) {
+      console.log( e.message, toSearch )
+    }
+  }
 
-  /*useEffect( () => {
+  useEffect( () => {
+    console.log( 'nuovo messaggio' )
     const chatHistory = document.getElementById( "chat-history" );
     if ( chatHistory )
       chatHistory.scrollTop = chatHistory.scrollHeight;
-  }, [messages] )*/
+  }, [messages] )
 
   return (
     <>
@@ -42,33 +44,36 @@ export const Chat = () => {
         </div>
         <div className="messages">
           <div className="messages-content" id="chat-history">
-            {/*
+            {
               messages.length === 0 ? <h3>Non ci sono messaggi qui...</h3> :
                 messages.map( ( message, idx ) =>
-                  <div key={idx} className={`message ${message.personal ? 'message-personal' : ''}`}>
-                    <Card key={idx}
+                  <div key={`messagio_${idx}`} className={`message ${message.personal ? 'message-personal' : ''}`}>
+                    <Card
                       title={message.title}
                       text={message.text}
                       imgUrl={message.imgUrl}
                       actionUrl={message.actionUrl}
                     />
-                  </div> )*/
+                  </div> )
             }
           </div>
         </div>
         <div className="message-box">
-          <input type="text" className="message-input" placeholder="Inserisci ricerca..."
+          <input type="text" className="message-input"
+            placeholder="Inserisci ricerca..."
             maxLength={50}
             value={search}
             onChange={e => setSearch( e.target.value )}
             autoComplete="off"
             onKeyDown={evt => {
               if ( evt.key === "Enter" ) {
-                // searchCurrent()
+                searchCurrent()
               }
             }}
           />
-          <button type="submit" className="message-submit" onClick={() => console.log( 'cerca' )}>Cerca</button>
+          <button type="submit" className="message-submit"
+            onClick={searchCurrent}
+          >Cerca</button>
         </div>
 
       </div>
